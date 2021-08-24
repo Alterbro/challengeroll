13c25ba981af436ca03666716947713277894551
Gin-cell <assasin1290@gmail.com>
2021-08-25 01:51:14 +0300

    1

--- js2.js
+++ js2.js
@@ -1,11 +1,10 @@
 const url = "https://api.atlasacademy.io/export/NA/basic_servant.json";
-async function gacharoll() {
-    
+async function getISS() {
     const response = await fetch(url);
     const servants = await response.json();
-    
-
-    
+    var default_rarity = 3
+    var selected_rarity = document.querySelector('input[name="rarity"]:checked');
+    var rarity = (selected_rarity === null) ? default_rarity : selected_rarity.value;
       function* generateRandomRareServant(servants, rarity){
       var used_servants = []
       while(true){
@@ -13,15 +12,14 @@ async function gacharoll() {
         if(servant.rarity <= rarity && !used_servants.includes(servant.name))
         {
           used_servants.push(servant.name);
-          yield 'Servant Name: ' + servant.name + ', ' + servant.rarity +' star' + '</br> ';
+          yield 'Name: ' + servant.name + ', ' + servant.rarity + ' star' + '</br>'+'</br>';
         }
       }
     }
-    var generator = generateRandomRareServant(servants, 3);
-
+    var generator = generateRandomRareServant(servants, rarity);
     var result = Array.from({length: 5}, () => generator.next().value).join('');
-    
     document.getElementById("Bros, this code...").innerHTML = result;
-    console.log(result);
 }
-gacharoll();
+document.getElementById('myButton').addEventListener('click', function(getISS) {
+    getISS.preventDefault();
+  });
