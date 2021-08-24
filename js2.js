755671ac50054e4aab54dfe275d53cc1fb4056c6
Gin-cell <assasin1290@gmail.com>
2021-08-25 02:12:55 +0300

    12

--- js2.js
+++ js2.js
@@ -1,5 +1,5 @@
 const url = "https://api.atlasacademy.io/export/NA/basic_servant.json";
-async function getISS() {
+async function rollgacha() {
     const response = await fetch(url);
     const servants = await response.json();
     var default_rarity = 3
@@ -20,6 +20,6 @@ async function getISS() {
     var result = Array.from({length: 5}, () => generator.next().value).join('');
     document.getElementById("Bros, this code...").innerHTML = result;
 }
-document.getElementById('myButton').addEventListener('click', function(getISS) {
-    getISS.preventDefault();
+document.getElementById('myButton').addEventListener('click', function(rollgacha) {
+    rollgacha.preventDefault();
   });
